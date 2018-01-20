14c80b95bf5988b225ca8b3453205c3271cf0ad2
jokuskay <tafanasyev@gmail.com>
2018-01-20 21:38:37 +0700

    complete categories screen

--- Repository.js
+++ Repository.js
@@ -10,11 +10,19 @@ export const Repository = {
     getCategories() {
         return fetch(URLS.CATEGORIES)
             .then(response => response.json())
+            .then(categories => categories.sort())
             .catch(error => console.error(error))
     },
 
-    getRandomFact() {
-        return fetch(URLS.RANDOM_FACT)
+    getRandomFact(category = null) {
+        let url;
+        if (category == null) {
+            url = URLS.RANDOM_FACT;
+        } else {
+            url = URLS.RANDOM_CATEGORY_FACT.replace("{category}", category);
+        }
+
+        return fetch(url)
             .then(response => response.json())
             .catch(error => console.error(error))
     },
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, FlatList } from "react-native";
+import {
+    StyleSheet,
+    View,
+    Text,
+    ListView,
+    ActivityIndicator,
+    FlatList,
+    TouchableHighlight,
+    TouchableOpacity
+} from "react-native";
+import { Repository } from "../Repository";
+
 
 export class CategoriesScreen extends Component {
 
@@ -7,8 +18,72 @@ export class CategoriesScreen extends Component {
         title: "Categories"
     };
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true
+        }
+    }
+
+    componentDidMount() {
+        this._loadCategories();
+    }
+
+    _loadCategories() {
+        this.setState({ isLoading: true });
+
+        Repository.getCategories()
+            .then(categories => {
+                this.setState({
+                    isLoading: false,
+                    categories: categories
+                });
+            })
+    }
+
+    _formatCategoryName(categoryName) {
+        return categoryName.charAt(0).toUpperCase() + categoryName.substr(1);
+    }
+
     render() {
-        return (<FlatList />);
+        const { navigate } = this.props.navigation;
+
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading}>
+                    <ActivityIndicator />
+                </View>
+            );
+        }
+
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    data={this.state.categories}
+                    keyExtractor={(item, index) => index}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity onPress={() => navigate("Random", item)}>
+                            <Text style={styles.listItem}>
+                                {this._formatCategoryName(item)}
+                            </Text>
+                        </TouchableOpacity>
+                    )}
+                />
+            </View>
+        );
     }
 
 }
+
+const styles = StyleSheet.create({
+    loading: {
+        flex: 1,
+        justifyContent: "center"
+    },
+    container: {
+
+    },
+    listItem: {
+        padding: 10
+    }
+})
