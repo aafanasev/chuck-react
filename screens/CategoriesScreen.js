68f57e220ca666b2e7b759a64fcf11d5c125ff6a
jokuskay <tafanasyev@gmail.com>
2018-01-22 00:57:28 +0700

    add fancy grid view into categories screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,6 +3,7 @@ import {
     StyleSheet,
     View,
     Text,
+    ImageBackground,
     ListView,
     ActivityIndicator,
     FlatList,
@@ -45,6 +46,12 @@ export class CategoriesScreen extends Component {
         return categoryName.charAt(0).toUpperCase() + categoryName.substr(1);
     }
 
+    _getBackgroundImageUrl(categoryKey) {
+        return {
+            uri: "https://picsum.photos/200?random"
+        };
+    }
+
     render() {
         const { navigate } = this.props.navigation;
 
@@ -59,17 +66,29 @@ export class CategoriesScreen extends Component {
         return (
             <View style={styles.container}>
                 <FlatList
+                    style={styles.list}
+                    numColumns="2"
                     data={this.state.categories}
                     keyExtractor={(item, index) => index}
                     renderItem={({ item }) => {
                         let name = this._formatCategoryName(item);
                         return (
-                            <TouchableOpacity onPress={() => navigate("Random", {
-                                categoryKey: item,
-                                categoryName: name
-                            })}>
-                                <Text style={styles.listItem}>{name}</Text>
-                            </TouchableOpacity>
+                            <View style={styles.item}>
+                                <TouchableOpacity
+                                    style={styles.itemTouch}
+                                    onPress={() => navigate("Random", {
+                                        categoryKey: item,
+                                        categoryName: name
+                                    })}>
+                                    <ImageBackground
+                                        style={styles.itemImage}
+                                        source={this._getBackgroundImageUrl(item)} >
+                                        <Text style={styles.itemText}>
+                                            {name}
+                                        </Text>
+                                    </ImageBackground>
+                                </TouchableOpacity>
+                            </View>
                         );
                     }}
                 />
@@ -87,7 +106,36 @@ const styles = StyleSheet.create({
     container: {
 
     },
-    listItem: {
+    list: {
         padding: 10
+    },
+    item: {
+        width: "50%",
+        aspectRatio: 1,
+    },
+    itemTouch: {
+        flex: 1,
+        margin: 10,
+        borderRadius: 5,
+        shadowOpacity: 0.5,
+        shadowColor: "#000",
+        shadowRadius: 5,
+        shadowOffset: { height: 5, width: 0 },
+        elevation: 5,
+    },
+    itemImage: {
+        flex: 1,
+        alignItems: "center",
+        borderRadius: 5,
+        overflow: "hidden",
+    },
+    itemText: {
+        position: "absolute",
+        bottom: 0,
+        width: "100%",
+        padding: 10,
+        backgroundColor: "#0006",
+        textAlign: "center",
+        color: "#fff"
     }
 })
