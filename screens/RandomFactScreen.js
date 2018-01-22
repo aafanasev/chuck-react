8b20bb1dc86385b57f3b70c7f09f3bd664cc0ab0
jokuskay <tafanasyev@gmail.com>
2018-01-22 23:50:52 +0700

    redesign fact screen

--- screens/RandomFactScreen.js
+++ screens/RandomFactScreen.js
@@ -5,7 +5,9 @@ import {
     Text,
     Button,
     Share,
-    ActivityIndicator
+    ActivityIndicator,
+    Alert,
+    ImageBackground
 } from "react-native";
 import { Repository } from "../Repository";
 
@@ -53,6 +55,32 @@ export class RandomFactScreen extends Component {
             })
     }
 
+    _getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    _getRandomImage() {
+        // the image name in require has to be known statically
+        // https://facebook.github.io/react-native/docs/images.html#static-image-resources
+
+        let image;
+        switch (this._getRandomNumber(1, 4)) {
+            case 1:
+                image = require("./img/chuck-1.jpg");
+                break;
+            case 2:
+                image = require("./img/chuck-2.jpg");
+                break;
+            case 3:
+                image = require("./img/chuck-3.jpg");
+                break;
+            case 4:
+                image = require("./img/chuck-4.jpg");
+                break;
+        }
+        return image;
+    }
+
     _showShareDialog() {
         Share.share(
             {
@@ -76,19 +104,33 @@ export class RandomFactScreen extends Component {
 
         return (
             <View style={styles.container}>
-                <Text style={styles.text}>
-                    {this.state.fact.value}
-                </Text>
-                <Button style={styles.button}
-                    title="One more"
-                    onPress={() => {
-                        this._loadFact();
-                    }} />
-                <Button style={styles.button}
-                    title="Share"
-                    onPress={() => {
-                        this._showShareDialog();
-                    }} />
+                <View style={styles.card}>
+                    <ImageBackground
+                        style={styles.cardBackground}
+                        source={this._getRandomImage()}>
+                        <Text style={styles.text}>
+                            {this.state.fact.value}
+                        </Text>
+                    </ImageBackground>
+                </View>
+
+                <View style={styles.actions}>
+                    <Button style={styles.favorite}
+                        title="Favorite"
+                        onPress={() => {
+                            Alert.alert("Not implemented yet")
+                        }} />
+                    <Button style={styles.random}
+                        title="One more"
+                        onPress={() => {
+                            this._loadFact();
+                        }} />
+                    <Button style={styles.share}
+                        title="Share"
+                        onPress={() => {
+                            this._showShareDialog();
+                        }} />
+                </View>
             </View>
         );
     }
@@ -98,19 +140,53 @@ export class RandomFactScreen extends Component {
 const styles = StyleSheet.create({
     loading: {
         flex: 1,
-        justifyContent: "center"
+        justifyContent: "center",
+        backgroundColor: "#eee",
     },
     container: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
-        padding: 20
+        backgroundColor: "#eee",
+        padding: 20,
+    },
+    card: {
+        width: "100%",
+        borderRadius: 5,
+        shadowOpacity: 0.5,
+        shadowColor: "#000",
+        shadowRadius: 5,
+        shadowOffset: { height: 5, width: 0 },
+        elevation: 5,
+    },
+    cardBackground: {
+        width: "100%",
+        padding: 30,
+        aspectRatio: 1,
+        borderRadius: 5,
+        overflow: "hidden",
+        justifyContent: "center",
     },
     text: {
-        marginBottom: 30,
-        textAlign: "center"
+        textAlign: "center",
+        color: "#fff",
+        fontWeight: "bold",
+        fontSize: 24,
+        textShadowColor: "#000",
+        textShadowRadius: 20,
+        textShadowOffset: { width: 0, height: 1 }
     },
-    button: {
+    actions: {
+        marginTop: 30,
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    random: {
+
+    },
+    favorite: {
+
+    },
+    share: {
 
     }
 })
